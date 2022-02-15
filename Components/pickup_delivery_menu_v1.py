8e96f672428932962e14d456aaec9fe014707942
danettp <17204@my.sanctamaria.school.nz>
2022-02-16 09:47:56 +1300

    Created pickup or delivery menu
    Created different versions of menu refining it in version 4.

---
+++ Components/pickup_delivery_menu_v1.py
@@ -0,0 +1,17 @@
+
+# Bugs
+# will only work for valid input "d" and "p"
+
+# menu so that user can choose either pickup or delivery
+
+print ("Do you want your order delivered or are you picking it up?")
+
+print("For delivery press enter d or enter p for pickup.")
+
+delivery = input()
+
+if delivery == "d":
+    print("Delivery")
+
+elif delivery == "p":
+    print("Pickup")
