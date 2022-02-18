14a6268493f776523fab029f4df453e9f6f3efaa
danettp <17204@my.sanctamaria.school.nz>
2022-02-18 15:04:23 +1300

    Created version 1 of pickup function
    Basic instructions and input for name and phone number

---
+++ Components/pickup_v1.py
@@ -0,0 +1,13 @@
+
+
+print("Please enter the  pickup information")
+
+#customer name 
+name = input ("Please enter your name ")
+
+
+#customer phone number
+phone = input("Please enter your phone number ")
+
+print(name)
+print(phone)
