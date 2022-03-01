8f4a216523aac56a6aac4ffd001c2d90af910245
danettp <17204@my.sanctamaria.school.nz>
2022-03-02 09:37:02 +1300

    Integrated delivery_v2 to main app
    Created delivery version 2 and integrated delivery info into main app.

--- pizza_bot.py
+++ pizza_bot.py
@@ -49,10 +49,11 @@ def order_type():
             if delivery >= 1 and delivery <= 2:
                 if delivery == 1:
                     print("Pickup")
-                    pickup()
+                    pickup_info()
                     break 
                 elif delivery == 2:
                     print("Delivery")
+                    delivery_info()
                     break
             else: 
                 print("The number must be 1 or 2")
@@ -61,26 +62,39 @@ def order_type():
             print("Please enter 1 or 2")
 
 
-
-
-
 # Pick up information - name and phone number
-def pickup():
+def pickup_info():
     question = ("Please enter your name ")
     customer_details["name"] = not_blank(question)
-    #print(customer_details["name"])
+    print(customer_details["name"])
 
     question = ("Please enter your phone number ")
     customer_details['phone'] = not_blank(question)
-    #print(customer_details["phone"])
     print(customer_details)
 
 
-
-
-
 # Delivery information - name address and phone
+def delivery_info():
+    question = ("Please enter your name ")
+    customer_details["name"] = not_blank(question)
+    print(customer_details["name"])
 
+    question = ("Please enter your phone number ")
+    customer_details['phone'] = not_blank(question)
+    print(customer_details["phone"])
+
+    question = ("Please enter your house number ")
+    customer_details['house'] = not_blank(question)
+    print(customer_details["house"])
+
+    question = ("Please enter your street name ")
+    customer_details['street'] = not_blank(question)
+    print(customer_details["street"])
+
+    question = ("Please enter your suburb ")
+    customer_details['suburb'] = not_blank(question)
+    print(customer_details["suburb"])
+    print(customer_details)
 
 
 
